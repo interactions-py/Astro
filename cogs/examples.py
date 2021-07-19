2e75ace57422a6d576e840e3e76b84a9ccd72ab3
Toricane <73972068+Toricane@users.noreply.github.com>
2021-07-19 10:44:16 -0700

    Display which button/select option was used
    Displays which button was pressed, and what options were selected in a select. Also removed `custom_id` from the `ButtonStyle.URL` button since URL buttons can't have a `custom_id`. Finally, replaced the component callbacks with a global event handler. Tested all of them and they work 100%.

--- cogs/examples.py
+++ cogs/examples.py
@@ -23,33 +23,32 @@ class Examples(commands.Cog):
     async def example_buttons(self, ctx: SlashContext):
         buttons = [
             create_button(
-                style=ButtonStyle.primary,
-                label="Primary/Blue/Blurple",
-                custom_id="button"
+                style=ButtonStyle.primary, # what colo[u]r (except URL)
+                label="Primary/Blue/Blurple", # what it displays
+                custom_id="Primary/Blue/Blurple" # its component id
             ),
             create_button(
                 style=ButtonStyle.secondary,
                 label="Secondary/Gr[a|e]y",
-                custom_id="button"
+                custom_id="Secondary/Gr[a|e]y"
             ),
             create_button(
                 style=ButtonStyle.success,
                 label="Success/Green",
-                custom_id="button"
+                custom_id="Success/Green"
             ),
             create_button(
                 style=ButtonStyle.danger,
                 label="Danger/Red",
-                custom_id="button"
+                custom_id="Danger/Red"
             ),
             create_button(
                 style=ButtonStyle.URL,
                 label="URL",
-                url="https://discord-py-slash-command.readthedocs.io/en/latest/components.html",
-                custom_id="button"
+                url="https://discord-py-slash-command.readthedocs.io/en/latest/components.html"
             )
         ]
-        action_row = create_actionrow(*buttons)
+        action_row = create_actionrow(*buttons) # creates action row of 5 buttons
         await ctx.send("All of the buttons:", components=[action_row])
     
     @cog_ext.cog_subcommand(base="example",
@@ -58,9 +57,9 @@ class Examples(commands.Cog):
     async def example_select(self, ctx: SlashContext):
         select = create_select(
             options=[# the options in your dropdown
-                create_select_option("Lab Coat", value="coat", emoji="🥼"),
-                create_select_option("Test Tube", value="tube", emoji="🧪"),
-                create_select_option("Petri Dish", value="dish", emoji="🧫"),
+                create_select_option("Lab Coat", value="Lab Coat", emoji="🥼"),
+                create_select_option("Test Tube", value="Test Tube", emoji="🧪"),
+                create_select_option("Petri Dish", value="Petri Dish", emoji="🧫"),
             ],
             placeholder="Choose your option",  # the placeholder text to show when no options have been chosen
             min_values=1,  # the minimum number of options a user must select
@@ -69,13 +68,13 @@ class Examples(commands.Cog):
         )
         await ctx.send("Example select:", components=[create_actionrow(select)])  # like action row with buttons but without * in front of the variable
     
-    @cog_ext.cog_component()
-    async def button(self, ctx: ComponentContext):
-        await ctx.send("You pressed a button!", hidden=True)
-    
-    @cog_ext.cog_component()
-    async def select(self, ctx: ComponentContext):
-        await ctx.send("You used a select!", hidden=True)
+    @commands.Cog.listener()
+    async def on_component(self, ctx: ComponentContext):
+        if ctx.component_type == 2: # button
+            await ctx.send(f"You pressed {ctx.custom_id}!", hidden=True)
+        else: # select
+            string = ' and '.join(ctx.selected_options) if len(ctx.selected_options) > 1 else ''.join(ctx.selected_options)
+            await ctx.send(f"You selected {string}!")
 
 def setup(bot):
     bot.add_cog(Examples(bot))
