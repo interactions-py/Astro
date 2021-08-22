0a9de519927faa7b7db6dc9b0314b5815216bc24
Toricane <73972068+Toricane@users.noreply.github.com>
2021-08-22 16:03:11 -0700

    Update paginator to 1.2.0
    pip install -U dinteractions-Paginator

--- cogs/paginator.py
+++ cogs/paginator.py
@@ -8,29 +8,44 @@ guild_ids = get_settings("servers")
 example1 = """\
 ```py
 import discord
-import discord_slash
+from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from dinteractions_Paginator import Paginator
 
+bot = commands.Bot(command_prefix="/")
+slash = SlashCommand(bot, sync_commands=True)
 
-@slash.slash(name="command")
-async def command(ctx: SlashContext):
-    embed1 = discord.Embed(title="title")
-    embed2 = discord.Embed(title="another title")
-    embed3 = discord.Embed(title="yet another title")
-    pages = [embed1, embed2, embed3]
-    await Paginator(bot=bot, ctx=ctx, pages=pages)
-    
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}!")
 
+@slash.slash(name="embeds")
+async def embeds(ctx: SlashContext):
+    one = discord.Embed(title="1st Embed", description="General Kenobi!", color=discord.Color.red())
+    two = discord.Embed(title="2nd Embed", description="General Kenobi!", color=discord.Color.orange())
+    three = discord.Embed(title="3rd Embed", description="General Kenobi!", color=discord.Color.gold())
+    four = discord.Embed(title="4th Embed", description="General Kenobi!", color=discord.Color.green())
+    five = discord.Embed(title="5th Embed", description="General Kenobi!", color=discord.Color.blue())
+    pages = [one, two, three, four, five]
+
+    await Paginator(bot=bot, ctx=ctx, pages=pages, content=["1", "2", "3", "4", "5"], timeout=60).run()
+
+bot.run("token")
 ```
 """
 example2 = """\
 ```py
 import discord
-import discord_slash
+from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from dinteractions_Paginator import Paginator
 
+bot = commands.Bot(command_prefix="/")
+slash = SlashCommand(bot, sync_commands=True)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}!")
 
 @slash.slash(name="command")
     async def command(ctx: SlashContext):
@@ -44,8 +59,8 @@ from dinteractions_Paginator import Paginator
         prevLabel="Back", nextLabel="Forward",
         prevEmoji="♥", nextEmoji="♥",
         prevStyle=1, nextStyle=2, 
-        indexStyle=3, timeout=10)
-
+        indexStyle=3, timeout=10).run()
+bot.run("token")
 ```
 """
 
@@ -89,7 +104,7 @@ class paginator(commands.Cog):
         pages = [p1, p2, p3, p4, p5, p6]
         await Paginator(
             bot=self.bot, ctx=ctx, pages=pages, content="Paginator example", timeout=60
-        )
+        ).run()
 
 
 def setup(bot):
