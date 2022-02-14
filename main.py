313cf53cbf26863acdb48567d3626c0276fdf5bb
James Walston <officialdeltadev@gmail.com>
2022-02-14 14:17:46 -0500

    feat: add `/info` and `Get user information` commands.

--- main.py
+++ main.py
@@ -1,99 +1,93 @@
+import interactions
+import dotenv
 import logging
-import discord
-import discord_slash
-from discord.ext import commands
-from discord_slash.utils import manage_commands
-from modules import page
-from modules import sphinx_parser
-from modules import sqlite_db
-from modules.get_settings import get_settings, sanity_check
 
-sanity_check()
-bot = commands.Bot(
-    command_prefix="/",
-    intents=discord.Intents.all(),
-    allowed_mentions=discord.AllowedMentions(everyone=False),
-    help_command=None,
-)
-slash = discord_slash.SlashCommand(bot, sync_commands=False)
-bot.db = sqlite_db.SQLiteDB("data")
-guild_ids = get_settings("servers")
-logger = logging.getLogger("discord")
-logging.basicConfig(level=logging.INFO)  # DEBUG/INFO/WARNING/ERROR/CRITICAL
-handler = logging.FileHandler(filename=f"slash.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
-logger.addHandler(handler)
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = dotenv.get_key(".env", "token")
+bot = interactions.Client(TOKEN, disable_sync=True)
 
 
-@slash.slash(
-    name="subscribe", guild_ids=guild_ids, description="Subscribes to new release."
-)
-async def _subscribe(ctx: discord_slash.SlashContext):
-    user = ctx.author
-    if [x for x in user.roles if x.id == get_settings("sub_role_id")]:
-        return await ctx.send("You are already subscribed!", hidden=True)
-    await user.add_roles(ctx.guild.get_role(get_settings("sub_role_id")))
-    await ctx.send("Successfully subscribed to new release!", hidden=True)
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.me.name}.")
 
 
-@slash.slash(
-    name="unsubscribe",
-    guild_ids=guild_ids,
-    description="Unsubscribes from new release.",
-)
-async def _unsubscribe(ctx: discord_slash.SlashContext):
-    user = ctx.author
-    if not [x for x in user.roles if x.id == get_settings("sub_role_id")]:
-        return await ctx.send("You are already unsubscribed!", hidden=True)
-    await user.remove_roles(ctx.guild.get_role(get_settings("sub_role_id")))
-    await ctx.send("Successfully unsubscribed to new release!", hidden=True)
-
-
-@slash.slash(
-    name="search",
-    guild_ids=guild_ids,
-    description="Searches the docs for the given text",
-    options=[manage_commands.create_option("text", "Text to search.", 3, True)],
-)
-async def _docs(ctx: discord_slash.SlashContext, text: str):
-    await ctx.defer()
-    base_url = "https://discord-interactions.readthedocs.io/en/latest/"
-    resp = await sphinx_parser.search_from_sphinx(base_url + "genindex.html", text)
-    if not resp:
-        return await ctx.send("No result found.")
-    base_embed = discord.Embed(
-        title="Document Search", color=discord.Color.from_rgb(225, 225, 225)
+@bot.user_command(name="Get user information")
+async def get_user_info(ctx: interactions.CommandContext):
+    embed = interactions.Embed(
+        title="User Information",
+        description="This is the retrieved information on the user.",
+        author=interactions.EmbedAuthor(
+            name=f"<@{ctx.target.user.id}>",
+            url=f"discord://-/users/{ctx.target.user.id}",
+            icon_url=f"https://cdn.discordapp.com/avatars/{ctx.target.user.id}/{ctx.target.avatar}.png",
+        ),
+        fields=[
+            interactions.EmbedField(
+                name="Username",
+                value=f"{ctx.target.user.username}#{ctx.target.user.discriminator}",
+                inline=True,
+            ),
+            interactions.EmbedField(name="ID", value=ctx.target.user.id, inline=True),
+            interactions.EmbedField(
+                name="Timestamps",
+                value="\n".join(
+                    [
+                        f"Joined: <t:{ctx.target.joined_at.utcnow()}:R>.",
+                        f"Created: <t:{ctx.target.id.timestamp.utcnow()}:R>.",
+                    ]
+                ),
+                inline=True,
+            ),
+            interactions.EmbedField(
+                name="Roles",
+                value=", ".join([f"`{role}`" for role in ctx.target.roles])
+                if isinstance(ctx.target, interactions.Member)
+                else "N/A",
+            ),
+            interactions.EmbedField(name=""),
+        ],
     )
-    base_embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
-    count = 1
-    embed_list = []
-    page_embed = base_embed.copy()
-    for_res = resp.copy()
-    for x in for_res:
-        if count != 1 and count % 5 == 1:
-            embed_list.append(page_embed)
-            page_embed = base_embed.copy()
-        link = base_url + x
-        try:
-            page_embed.add_field(
-                name=str(count), value=f"[`{x.split('#')[1]}`]({link})", inline=False
-            )
-        except IndexError:
-            continue
-        resp.remove(x)
-        count += 1
-    embed_list.append(page_embed)
-    if not embed_list:
-        return await ctx.send("No result found.")
-    await page.start_page(bot, ctx, embed_list, embed=True)
+    await ctx.send(embeds=embed, ephemeral=True)
 
 
-bot.load_extension("cogs.git")
-bot.load_extension("cogs.language")
-bot.load_extension("cogs.tester")
-bot.load_extension("cogs.tags")
-bot.load_extension("cogs.buttons")
-bot.load_extension("cogs.examples")
-bot.run(get_settings("token"))
+@bot.command(name="info", description="Get information about the bot.")
+async def info(ctx: interactions.CommandContext):
+    embed = interactions.Embed(
+        title="Info",
+        footer=interactions.EmbedFooter(
+            text="This bot is maintained by the interactions.py ext team."
+        ),
+        fields=[
+            interactions.EmbedField(
+                name="What is this for?",
+                value="".join(
+                    [
+                        "Astro is the main bot powering moderation and other utilities in the interactions.py support server.",
+                        " The goal of Astro is to make searching simple, and automate our moderation process. Whether that's creating tags with",
+                        " autocompleted suggestions, code examples, or interactive tutorials: Astro has you covered. Interactions should be simple to understand,",
+                        " and coding them is no different.",
+                    ]
+                ),
+            ),
+            interactions.EmbedField(
+                name="What does this bot run on?",
+                value="".join(
+                    [
+                        "This project is built with interactions.py `4.0.2`, the no. 1 leading Python interactions library that",
+                        " empowers bots with the ability to implement slash commands and components with ease. The codebase of this bot reflects how simple,",
+                        " modular and scalable the library is---staying true to the motto of what it does.",
+                    ]
+                ),
+            ),
+            interactions.EmbedField(
+                name="How can I contribute to this bot?",
+                value="Please check out the official GitHub repository which holds the source code of this bot here: https://github.com/interactions-py/Astro",
+            ),
+        ],
+    )
+    await ctx.send(embeds=embed)
+
+
+bot.start()
