8213d544e4078fead1ec7b58cf40b70b2f762c55
fl0w <officialdeltadev@gmail.com>
2022-03-07 00:28:00 -0500

    style: move tag command structure elsewhere

--- src/exts/tag.py
+++ src/exts/tag.py
@@ -2,6 +2,7 @@ import datetime
 import interactions
 import json
 import logging
+import src.cmds.tag
 import src.const
 import src.model
 
@@ -14,81 +15,7 @@ class Tag(interactions.Extension):
         self.bot = bot
         self.edited_name = None
 
-    @interactions.extension_command(
-        name="tag",
-        description="Handles \"tags\" aka. pre-written feeds for help.",
-        scope=src.const.METADATA["guild"],
-        options=[
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="view",
-                description="Views an existing tag.",
-                options=[
-                    interactions.Option(
-                        type=interactions.OptionType.STRING,
-                        name="name",
-                        description="The name of the tag you wish to view.",
-                        required=True,
-                        autocomplete=True,
-                    ),
-                ],
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="info",
-                description="Gathers information about a tag.",
-                options=[
-                    interactions.Option(
-                        type=interactions.OptionType.STRING,
-                        name="name",
-                        description="The name of the tag you wish to gather information on.",
-                        required=True,
-                        autocomplete=True,
-                    )
-                ]
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="list",
-                description="Lists all existing tags.",
-                options=[],
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="create",
-                description="Creates a new tag.",
-                options=[],
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="edit",
-                description="Edits an existing tag.",
-                options=[
-                    interactions.Option(
-                        type=interactions.OptionType.STRING,
-                        name="name",
-                        description="The name of the tag you wish to edit.",
-                        required=True,
-                        autocomplete=True,
-                    ),
-                ],
-            ),
-            interactions.Option(
-                type=interactions.OptionType.SUB_COMMAND,
-                name="delete",
-                description="Deletes an existing tag.",
-                options=[
-                    interactions.Option(
-                        type=interactions.OptionType.STRING,
-                        name="name",
-                        description="The name of the tag you wish to delete.",
-                        required=True,
-                        autocomplete=True,
-                    ),
-                ],
-            ),
-        ],
-    )
+    @interactions.extension_command(**src.cmds.tag.cmd)
     async def tag(
         self, ctx: interactions.CommandContext, sub_command: str=None, name: str=None
     ):
