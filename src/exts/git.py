31e203a772d04651244a788d13d77d318cc91363
Nan R <88463477+Nanrech@users.noreply.github.com>
2022-04-16 09:58:08 +0200

    Update src/exts/git.py
    Co-authored-by: james <41456914+jameswalston@users.noreply.github.com>

--- src/exts/git.py
+++ src/exts/git.py
@@ -132,7 +132,7 @@ class Git(interactions.Extension):
         if res['state'] == "closed":
             if "pull_request" in res.keys():
                 if res['pull_request']['merged_at']:
-                    description = description + f"• Merged: <t:{mrg_at}:R> by {res['closed_by']['login']}\n"
+                    description += f"• Merged: <t:{mrg_at}:R> by {res['closed_by']['login']}\n"
                     return description
             description = description + f"• Closed: <t:{cls_at}:R> by {res['closed_by']['login']}"
         return description
