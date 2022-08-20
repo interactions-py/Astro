2d644709f521b51351c0ee766237e061692d8abc
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-08-20 17:12:03 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -213,6 +213,11 @@ async def letmegooglethat(ctx: interactions.CommandContext, param: str):
         return await ctx.send(":x: You are not a helper.", ephemeral=True)
     
     params = param.split(" ") 
-    q: str = "+".join(word for word in param.split (" ") 
+    q: str = "+".join(word for word in param.split(" ") 
     await ctx.send("collecting Google things..."), ephemeral=True) 
     await (await ctx.get_channel()).send(f"https://letmegooglethat.com/?q={q}")
+
+@bot.command(scope=METADATA["guild"] 
+@interactions.option()
+async def docs_search(ctx: interactions.CommandContext, query: str):
+    await ctx.send(f"https://interactionspy.readthedocs.io/en/latest/search.html?q={'+'.join(word for word in query.split(" ")}&check_keywords=yes&area=default"
