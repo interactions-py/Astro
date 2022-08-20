d09e2af67c5941a159464e26574f1461fb9cfe77
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-08-20 17:16:54 +0200

    Fuck parenthesis

--- src/bot.py
+++ src/bot.py
@@ -217,7 +217,7 @@ async def letmegooglethat(ctx: interactions.CommandContext, param: str):
     await ctx.send("collecting Google things...", ephemeral=True) 
     await (await ctx.get_channel()).send(f"https://letmegooglethat.com/?q={q}")
 
-@bot.command(scope=METADATA["guild"] 
+@bot.command(scope=METADATA["guild"]) 
 @interactions.option()
 async def docs_search(ctx: interactions.CommandContext, query: str):
     await ctx.send(f"https://interactionspy.readthedocs.io/en/latest/search.html?q={'+'.join(word for word in query.split(" ")}&check_keywords=yes&area=default"
