0f6716079f25fc62a0581868a07804d6f6c18ed0
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-08-20 17:15:40 +0200

    ²

--- src/bot.py
+++ src/bot.py
@@ -214,7 +214,7 @@ async def letmegooglethat(ctx: interactions.CommandContext, param: str):
     
     params = param.split(" ") 
     q: str = "+".join(word for word in param.split(" "))
-    await ctx.send("collecting Google things..."), ephemeral=True) 
+    await ctx.send("collecting Google things...", ephemeral=True) 
     await (await ctx.get_channel()).send(f"https://letmegooglethat.com/?q={q}")
 
 @bot.command(scope=METADATA["guild"] 
