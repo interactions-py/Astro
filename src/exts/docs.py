dd83fa237090c7253f7c959924ce4f8c537a1064
ItsRqtl <itsrqtl@gmail.com>
2022-12-03 04:03:14 -0800

    fix more stuff that I messed up

--- src/exts/docs.py
+++ src/exts/docs.py
@@ -1,6 +1,5 @@
 from urllib.parse import quote
 
-import aiohttp
 import interactions
 from interactions.ext.paginator import Page, Paginator
 
@@ -17,31 +16,42 @@ class Docs(interactions.Extension):
         """Search interactions.py's documentation."""
         if search is None:
             return await ctx.send("https://interactionspy.readthedocs.io/en/latest/index.html")
+
         url = f"https://interactionspy.readthedocs.io/_/api/v2/search/?q={quote(search)}&project=interactionspy&version=latest&language=en"
         async with self.client._http._req._session.get(url) as resp:
             data = await resp.json()
+
         if data["count"] == 0:
             return await ctx.send("No results found.")
+
         results = []
         for i in data["results"]:
             eb = interactions.Embed(title=i["title"], url=f"{i['domain']}{i['path']}")
             for j in i["blocks"]:
                 if j["type"] == "domain":
+                    content = j["highlight"]["content"]
+                    highlight = (
+                        j["highlight"]["content"].replace("<span>", "").replace("</span>", "")
+                    )
+
+                    if j["content"].startswith(highlight):
+                        content = f"... {content}"
+                    if j["content"].endswith(highlight):
+                        content = f"{content} ..."
+
                     content = (
-                        j["content"]
-                        .replace("*", "\\*")
+                        content.replace("*", "\\*")
                         .replace("_", "\\_")
                         .replace("`", "\\`")
                         .replace("~", "\\~")
+                        .replace("<span>", "**")
+                        .replace("</span>", "**")
                     )
-                    link = f"""\n[[Link]({i["domain"]}{i["path"]}#{j["id"]})]"""
-                    if len(content) + len(link) > 1024:
-                        content = f"{content[:1020 - len(link)]}..."
-                    content += link
                     eb.add_field(name=j["name"], value=content)
             results.append(eb)
+
         if len(results) == 1:
-            return await ctx.send(embed=results[0])
+            return await ctx.send(embeds=results[0])
         await Paginator(self.client, ctx, pages=[Page(embeds=i) for i in results]).run()
 
 
