f26fa6be91e683f61df5ec5417121cd73c7c1724
Sophia <41456914+i0bs@users.noreply.github.com>
2022-12-18 15:03:00 -0500

    style(help): modify help thread creation titles
    So since we're using an "autogenerated" tag in #help, we can instead make the modal title truncate whatever given message it was.

--- exts/help.py
+++ exts/help.py
@@ -34,7 +34,7 @@ class HelpChannel(naff.Extension):
             [
                 naff.ShortText(
                     label="What should the thread be named?",
-                    value=f"[AUTO] Help thread for {message.author.username}",
+                    value=message.content[:40] + "..."
                     min_length=1,
                     max_length=100,
                     custom_id="help_thread_name",
